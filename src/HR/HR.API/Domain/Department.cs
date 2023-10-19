e541ed3d477b8bb2f7c71f7c86ec8f99f4b5c07e
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-10-19 22:49:02 +0100

    add department model

---
+++ src/HR/HR.API/Domain/Department.cs
@@ -0,0 +1,15 @@
+﻿using Shared.Entities;
+
+namespace HR.API.Domain;
+
+public class Department : EntityBase
+{
+    [BsonElement("Name")]
+    public required string Name { get; set; }
+    public int NoOfEmployees { get; set; }
+    [BsonElement("Description")]
+    public string Description { get; set; }
+
+    [BsonElement("Manager")]
+    public string Manager { get; set; }
+}
