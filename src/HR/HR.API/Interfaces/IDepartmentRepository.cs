e6515fdf30754f39726feb09ab7b872ba535ca13
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-10-21 07:28:20 +0100

    add department service

---
+++ src/HR/HR.API/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace HR.API.Interfaces;
+
+public interface IDepartmentRepository
+{
+    Task<BaseResponse<GetDepartmentDTO>> CreateDepartmentAsync(CreateDepartmentDTO department);
+    Task<BaseResponse<IEnumerable<GetDepartmentDTO>>> GetAllDepartmentAsync();
+}
---
+++ src/HR/HR.API/Repository/DepartmentRepository.cs
@@ -0,0 +1,34 @@
+﻿namespace HR.API.Repository;
+
+public class DepartmentRepository : MongoRepository<Department>, IDepartmentRepository
+{
+    // needs docker add in doc
+    public DepartmentRepository(IMongoDatabase database, string collectionName = "departments") : base(database, collectionName)
+    {
+    }
+
+    public async Task<BaseResponse<GetDepartmentDTO>> CreateDepartmentAsync(CreateDepartmentDTO departmentDto)
+    {
+        ArgumentNullException.ThrowIfNull(departmentDto);
+
+        var department = new Department
+        {
+            Name = departmentDto.Name,
+            Manager = departmentDto.Manager,
+            NoOfEmployees = departmentDto.NoOfEmployee,
+            Description = departmentDto.Description
+        };
+
+        await CreateAsync(department);
+        return new BaseResponse<GetDepartmentDTO>(department.AsDto());
+    }
+
+    public async Task<BaseResponse<IEnumerable<GetDepartmentDTO>>> GetAllDepartmentAsync()
+    {
+        var data = (await GetAllAsync())
+            .Select(x => x.AsDto());
+
+        return new BaseResponse<IEnumerable<GetDepartmentDTO>>(data);
+    }
+
+}
