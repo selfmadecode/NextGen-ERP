35a24778d233cdafd408f207728632f9f24aafa6
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-10-21 07:29:23 +0100

    UPDATE IMPLEMENTATION

---
+++ src/HR/HR.API/Data/SeedDepartmentContext.cs
@@ -0,0 +1,31 @@
+﻿using HR.API.Domain;
+using MongoDB.Driver;
+
+namespace HR.API.Data;
+
+public class SeedDepartmentContext
+{
+    public static async Task SeedDepartment(IMongoCollection<Department> departmentCollection)
+    {
+        bool departmentExist = departmentCollection.Find(p => true).Any();
+
+        if (!departmentExist)
+            await departmentCollection.InsertManyAsync(GetDepartmentData());
+
+    }
+
+    private static IEnumerable<Department> GetDepartmentData()
+    {
+        return new List<Department>()
+        {
+            new Department()
+            {
+                Manager = "John Doe",
+                Name = "Procurement",
+                Id = Guid.NewGuid(),
+                Description = "Procurement",
+                NoOfEmployees = 0
+            }
+        };
+    }
+}
