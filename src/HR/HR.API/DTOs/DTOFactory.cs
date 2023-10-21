c500e45d0aa25451ad8ba53d89f07aa3c4d04577
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-10-21 01:37:21 +0100

    add DTO class

---
+++ src/HR/HR.API/DTOs/DTOFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace HR.API.DTOs;
+
+public static class DTOFactory
+{
+    public static GetDepartmentDTO AsDto(this Department department)
+    {
+        return new GetDepartmentDTO(department.Id, department.Name, department.Manager, department.NoOfEmployees, department.Description);
+    }
+
+}
