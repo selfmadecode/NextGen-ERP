9f71b1b3ae4948b002d9b3a7c2af5c2acaa7b1c1
successJohn <successojohn@gmail.com>
2023-12-18 13:02:58 +0100

    Mass Transit Initial Setup

---
+++ src/SelfServicePortal.API/DTO/PublishedEmployeeDTO.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using Shared;
+
+namespace SelfServicePortal.API.DTO
+{
+    public sealed class PublishedEmployeeDTO : IConsumer<CreatedEvent>
+    {
+       public async Task Consume(ConsumeContext<CreatedEvent> context)
+        {
+            List<CreatedEvent> Employees = new List<CreatedEvent>();
+
+            Employees.Add(context.Message);
+        }
+    }
+}
--- src/SelfServicePortal.API/Program.cs
+++ src/SelfServicePortal.API/Program.cs
@@ -1,3 +1,6 @@
+using MassTransit;
+using SelfServicePortal.API.DTO;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMassTransit(busConfigurator =>
+{
+    busConfigurator.SetKebabCaseEndpointNameFormatter();
+    busConfigurator.AddConsumer<PublishedEmployeeDTO>();
+    busConfigurator.UsingRabbitMq((context, configurator) =>
+    {
+        configurator.Host(new Uri("rabbitmq://localhost"), h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+
+        });
+
+        configurator.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
