2cebd581dc6c36260149455432e6ede486f7729e
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-12-24 09:10:32 +0100

    cleanup: update comments

--- src/Shared/EventBus/RegisterEventBus.cs
+++ src/Shared/EventBus/RegisterEventBus.cs
@@ -7,13 +7,10 @@ public static class RegisterEventBus
 {
     public static IServiceCollection AddMassTransitWithRabbitMq(this IServiceCollection services, IConfiguration configuration)
     {
-        // Retrieve RabbitMQ settings from the configuration
         var rabbitmqSettings = configuration.GetSection("RabbitMq").Get<RabbitMqSetting>();
 
-        // Configure MassTransit services
         services.AddMassTransit(busConfigurator =>
         {
-            // Set the endpoint name formatter to kebab case
             busConfigurator.SetKebabCaseEndpointNameFormatter();
 
             // Add consumers from the entry assembly (assembly of the application)
@@ -29,7 +26,6 @@ public static class RegisterEventBus
                     h.Password(rabbitmqSettings.Password);
                 });
 
-                // Configure MassTransit endpoints based on the context
                 configurator.ConfigureEndpoints(context);
             });
         });
