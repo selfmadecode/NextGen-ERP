ccfa2a4e14a9c6ea0822dae03bfdab4a97a4debd
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-03-25 20:58:31 +0100

    feat: Implement ApplicationDbContext class override onModelCreating method to apply entity configurations

--- src/Auth/Auth/Context/ApplicationDbContext.cs
+++ src/Auth/Auth/Context/ApplicationDbContext.cs
@@ -1,6 +1,22 @@
-﻿namespace Auth.Context
-{
-    public class ApplicationDbContext
+﻿using System.Reflection.Emit;
+using System.Reflection;
+using Auth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Context;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{   
+    public ApplicationDbContext(DbContextOptions options): base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
+
+
