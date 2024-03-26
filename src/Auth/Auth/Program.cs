67263152287b899696d039ef03c583f0cd4cfedb
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-03-26 10:36:58 +0100

    register openiddict, identity and dbcontext

--- src/Auth/Auth/Program.cs
+++ src/Auth/Auth/Program.cs
@@ -1,3 +1,5 @@
+using Auth.Helper.ServiceExtensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCustomDbContext(builder.Configuration);
+builder.Services.AddCustomIdentity(builder.Configuration);
+builder.Services.ConfigureOpenIdDict();
 
 var app = builder.Build();
 
