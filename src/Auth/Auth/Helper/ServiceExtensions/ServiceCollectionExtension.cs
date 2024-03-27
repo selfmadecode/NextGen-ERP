291605886798bdfa20cd1103b2b38c77e3edfb39
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-03-27 12:05:24 +0100

    clean up openId dict registration

--- src/Auth/Auth/Helper/ServiceExtensions/ServiceCollectionExtension.cs
+++ src/Auth/Auth/Helper/ServiceExtensions/ServiceCollectionExtension.cs
@@ -17,7 +17,6 @@ public static class ServiceCollectionExtension
         .AddCore(options =>
         {
             // Configure OpenIddict to use the Entity Framework Core stores and models.
-            // Note: call ReplaceDefaultEntities() to replace the default OpenIddict entities.
             options.UseEntityFrameworkCore()
                     .UseDbContext<ApplicationDbContext>();
         })
@@ -33,19 +32,15 @@ public static class ServiceCollectionExtension
             options.AddEncryptionKey(new SymmetricSecurityKey(
                 Convert.FromBase64String("DRjd/GnduI3Efzen9V9BvbNUfc/VKgXltV7Kbk9sMkY=")));
 
-            //// Register the signing and encryption credentials.
-            options.AddDevelopmentEncryptionCertificate()
-                .AddDevelopmentSigningCertificate();
-
-            //////////////////////////////////////
             //// Enable the token endpoint.
-            options.SetTokenEndpointUris("connect/token");
-
-            //// Enable the password flow.
-            options.AllowPasswordFlow();
-
-            //// Accept anonymous clients (i.e clients that don't send a client_id).
-            options.AcceptAnonymousClients();
+            options.SetTokenEndpointUris("connect/token")
+            .AllowPasswordFlow()//// Enable the password flow.
+            .AcceptAnonymousClients() // Accept anonymous clients (i.e clients that don't send a client_id)
+            .AddDevelopmentEncryptionCertificate() // Register the signing and encryption credentials.
+            .AddDevelopmentSigningCertificate()
+            .SetAccessTokenLifetime(TimeSpan.FromMinutes(60))
+            .SetIdentityTokenLifetime(TimeSpan.FromMinutes(60))
+            .SetRefreshTokenLifetime(TimeSpan.FromMinutes(120));
 
             //// Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
             options.UseAspNetCore()
@@ -57,19 +52,12 @@ public static class ServiceCollectionExtension
         {
             // Note: the validation handler uses OpenID Connect discovery
             // to retrieve the issuer signing keys used to validate tokens.
-            options.SetIssuer("https://localhost:7202/");
+            options.SetIssuer("https://localhost:7123/");
 
-            // Register the encryption credentials. This sample uses a symmetric
-            // encryption key that is shared between the server and the API project.
-            //
-            // Note: in a real world application, this encryption key should be
-            // stored in a safe place (e.g in Azure KeyVault, stored as a secret).
-            options.AddEncryptionKey(new SymmetricSecurityKey(
-                Convert.FromBase64String("DRjd/GnduI3Efzen9V9BvbNUfc/VKgXltV7Kbk9sMkY=")));
+            //options.AddEncryptionKey(new SymmetricSecurityKey(
+              //  Convert.FromBase64String("DRjd/GnduI3Efzen9V9BvbNUfc/VKgXltV7Kbk9sMkY=")));
 
-            // Register the System.Net.Http integration.
             options.UseSystemNetHttp();
-            /////////////////////////////////////////////////////
 
             //// Import the configuration from the local OpenIddict server instance.
             options.UseLocalServer();
