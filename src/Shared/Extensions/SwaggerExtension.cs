679692309ed196451bf0977929805c868cdb713a
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-03-28 21:36:34 +0100

    add swagger extention method

--- src/Shared/Extensions/SwaggerExtension.cs
+++ src/Shared/Extensions/SwaggerExtension.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.OpenApi.Models;
 
-namespace Shared.Extensions
+namespace Shared.Extensions;
+
+public static class SwaggerExtension
 {
-    internal class SwaggerExtension
+    public static void AddSwagger(this IServiceCollection services, string title)
     {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo 
+            { 
+                Title = title,
+                Version = "v1",
+                Contact = new OpenApiContact
+                {
+                    Name = "NextGen Admin",
+                    Email = "Info@nextgen.com"
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "MIT License",
+                    Url = new Uri("https://en.wikipedia.org/wiki/MIT_Lincense")
+                }
+            });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using Bearer scheme. \r\n\r\n" +
+                "Enter 'Bearer' [space] and then your token in the input below.\r\n\r\n" +
+                "Example: \"Bearer 123456\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                    },
+                    new string[] {}
+                }
+            });
+
+        });
     }
 }
