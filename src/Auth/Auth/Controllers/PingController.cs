4688ad6b597dc5fa30b9173cfdea556723537cf8
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-03-28 10:46:05 +0100

    add ping controller in auth

---
+++ src/Auth/Auth/Controllers/PingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PingController : ControllerBase
+{
+    [HttpGet]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public IActionResult Ping()
+    {
+        return Ok("--> Ping Ok");
+    }
+}
