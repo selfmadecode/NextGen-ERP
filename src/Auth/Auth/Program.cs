22f9ed144c88da23632fe0024f8e6a6e6abcc07b
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-03-29 02:50:54 +0100

    add swagger auth config

--- src/Auth/Auth/Program.cs
+++ src/Auth/Auth/Program.cs
@@ -1,15 +1,19 @@
 using Auth.Helper.ServiceExtensions;
+using Shared.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwagger("NextGen Auth API");
 
 builder.Services.AddCustomDbContext(builder.Configuration);
 builder.Services.ConfigureOpenIdDict(builder.Configuration);
 builder.Services.AddCustomIdentity(builder.Configuration);
 
+
+builder.Services.AddAuthorization();
 
 
 
