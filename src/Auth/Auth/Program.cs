8a93b4a5312cb9a7f4c9e46159239849add7de32
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-04-02 20:53:27 +0100

    update auth api swagger name

--- src/Auth/Auth/Program.cs
+++ src/Auth/Auth/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.ConfigureSwagger("NextGen Auth API");
+builder.Services.ConfigureSwagger("NextGen Authentication API");
 
 builder.Services.AddCustomDbContext(builder.Configuration);
 builder.Services.ConfigureOpenIdDict(builder.Configuration);
