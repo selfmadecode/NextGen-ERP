9a2372bcb0f69fd85998e91a8b142d012c083c43
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-04-05 22:06:52 +0100

    add entity base

---
+++ src/Shared/Common/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿namespace Shared.Common;
+
+public class EntityBase : IAuditable
+{
+    public Guid Id { get; set; }
+
+    public EntityBase()
+    {
+        CreatedOn = DateTime.UtcNow;
+    }
+
+    public DateTime CreatedOn { get; set; }
+    public bool IsDeleted { get; set; }
+    public Guid? DeletedBy { get; set; }
+    public DateTime? ModifiedOn { get; set; }
+    public Guid? ModifiedBy { get; set; }
+    public DateTime? DeletedOn { get; set; }
+}
