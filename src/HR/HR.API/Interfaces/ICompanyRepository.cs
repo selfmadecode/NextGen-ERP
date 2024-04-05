f54978aa5d308d5fbc83249e3be23c969f581448
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-04-05 21:40:15 +0100

    remove company repo

--- src/HR/HR.API/Interfaces/ICompanyRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using MongoDB.Driver;
-
-namespace HR.API.Interfaces;
-
-public interface ICompanyRepository
-{
-    Task AddEmployeeToDepartmentAndCompany(Employee employee, Guid departmentId, Guid companyId);
-
-}
