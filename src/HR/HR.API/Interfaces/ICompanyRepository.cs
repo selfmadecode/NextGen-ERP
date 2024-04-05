5bbd07c381aeb8ba4511711999ad2e7d3f7d211a
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-04-05 21:27:19 +0100

    add company repository interface

---
+++ src/HR/HR.API/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Driver;
+
+namespace HR.API.Interfaces;
+
+public interface ICompanyRepository
+{
+    Task AddEmployeeToDepartmentAndCompany(Employee employee, Guid departmentId, Guid companyId);
+
+}
