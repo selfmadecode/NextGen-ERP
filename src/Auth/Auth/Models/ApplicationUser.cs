090b6af668cf543449cba990e9ebe8ff32b160db
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-04-06 20:14:07 +0100

    add audited entity properties in appuser

--- src/Auth/Auth/Models/ApplicationUser.cs
+++ src/Auth/Auth/Models/ApplicationUser.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Shared.Common;
+namespace Auth.Models;
 
-namespace Auth.Models
+public class ApplicationUser : IdentityUser, IAuditable
 {
-    public class ApplicationUser : IdentityUser
-    {
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public DateTime? LastLoginDate { get; set; }
-
-        public Guid? CompanyId { get; set; }
-
-        public int MyProperty { get; set; }
-    }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public DateTime? LastLoginDate { get; set; }
+    public Guid? CompanyId { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public bool IsDeleted { get; set; }
+    public Guid? DeletedBy { get; set; }
+    public DateTime? ModifiedOn { get; set; }
+    public Guid? ModifiedBy { get; set; }
+    public DateTime? DeletedOn { get; set; }
 }
