7cdebccc8c8a009a4c84d7793deeafe9ad656ed7
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-07-05 23:09:30 +0100

    remove auth

--- src/Auth/Auth/Ultilities/IdentitySettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Auth.Ultilities;
-
-public class IdentitySettings
-{
-    public int LockoutMinutes { get; set; }
-    public int MinimumPasswordLength { get; set; }
-    public int MaxLockoutAttempt { get; set; }
-}
